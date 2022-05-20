90ff7436c95bf101e49191d7e6eefe026efe0830
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-05-20 11:34:23 -0500

    Update: semistandard fix

--- 0x0C-Javascript_advanced/0-welcome.js
+++ 0x0C-Javascript_advanced/0-welcome.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
 /**
  * welcome - lexical scope example
- * @param {String} firstName 
- * @param {String} lastName 
+ * @param {String} firstName
+ * @param {String} lastName
  */
-function welcome(firstName, lastName){
-    let fullName = firstName + " " + lastName;
-    function displayfullName(){
-        console.log(`Welcome ${fullName} !`);
-    }
-    displayfullName();
+function welcome (firstName, lastName) {
+  const fullName = `${firstName} ${lastName}`;
+  function displayfullName () {
+    console.log(`Welcome ${fullName} !`);
+  }
+  displayfullName();
 }
 
 welcome('Holberton', 'School');
