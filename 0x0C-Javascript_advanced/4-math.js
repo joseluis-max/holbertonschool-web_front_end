1f634b6b925583e56f0c719ce3832105def742bb
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-05-20 20:56:24 -0500

    Add: math

---
+++ 0x0C-Javascript_advanced/4-math.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+function divideBy (firstNumber) {
+  return (secondNumber) => secondNumber / firstNumber;
+}
+
+function addBy (firstNumber) {
+  return (secondNumber) => firstNumber + secondNumber;
+}
+
+const addBy100 = addBy(100);
+const addBy1000 = addBy(1000);
+const divideBy10 = divideBy(10);
+const divideBy100 = divideBy(100);
